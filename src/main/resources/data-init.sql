51af086ecabf5dd50d2f4a4b9e0500365980fb41
fbansept <bansept.franck@gmail.com>
2025-11-26 10:06:48 +0100

    ajout json view

--- src/main/resources/data-init.sql
+++ src/main/resources/data-init.sql
@@ -2,18 +2,21 @@ INSERT INTO famille (designation) VALUES
         ('Vin blanc'),
         ('Vin rouge');
 
-
-INSERT INTO produit (nom, prix, famille_id) VALUES
-        ('Reisling', 12.5, 1),
-        ('Coteau du Layon', 8.5, 1),
-        ('Haut Medoc', 10.2, 2);
-
 INSERT INTO fournisseur (nom) VALUES
-          ('Fournisseur 1'),
-          ('Fournisseur 2');
+      ('Fournisseur 1'),
+      ('Fournisseur 2');
 
-INSERT INTO produit_fournisseur (fournisseur_id, produit_id, prix_achat) VALUES
-      (1, 1, 10),
-      (1, 3, 10.8),
-      (2, 2 , 5),
-      (2, 3, 12);
+INSERT INTO produit (nom, prix, famille_id, fournisseur_id) VALUES
+        ('Reisling', 12.5, 1, 1),
+        ('Coteau du Layon', 8.5, 1, 2),
+        ('Haut Medoc', 10.2, 2, 2);
+
+INSERT INTO commande (date) VALUES
+          (NOW()) , ('2025-10-26 08:34:14.000000');
+
+INSERT INTO ligne_commande (commande_id, produit_id , quantite, prix_total) VALUES
+         (1, 2, 6, 75),
+         (1, 3, 2, 20.4),
+         (2, 2, 1, 10.4);
+
+
