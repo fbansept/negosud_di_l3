5b6ca9a9290f1ef00abb5dff071c07a06dea7653
fbansept <bansept.franck@gmail.com>
2026-03-02 14:49:48 +0100

    fix merge

--- src/main/java/edu/ban7/negosud_di_l3/model/Produit.java
+++ src/main/java/edu/ban7/negosud_di_l3/model/Produit.java
@@ -27,7 +27,7 @@ public class Produit {
     @NotBlank
     protected String nom;
 
-    @DecimalMin(value = "0.01", inclusive = true)
+    @DecimalMin(value = "0.01")
     protected float prix;
 
     //arrondir au centime supérieur
